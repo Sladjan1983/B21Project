bb643c055bfa16fd05dfd79b7bf1ebf3d4c6d0c5
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2020-11-10 12:26:34 -0500

    added new code 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         // B21 is good at IntelliJ
+        //Mac users and windows users a re happy
     }
 }
